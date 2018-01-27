d07b90534ed011a3c56da4d84df922d89bf4e1c7
Preston Bayle <prestonbayle@gmail.com>
2018-01-27 13:45:39 -0700

    finished except for black diamond

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Calculator from './compenents/Calculator/Calculator'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Calculator />
+      </div>
+    );
+  }
+}
+
+export default App;
